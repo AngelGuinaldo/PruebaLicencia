2b33e7ed9464334cd4920bb8452a2537aae07188
aguinaldogonzalez <aguinaldogonzalez@nautilus4.danielcastelao.org>
2019-01-15 09:48:38 +0100

    c.

---
+++ src/hola/mundo/HolaMundo.java
@@ -0,0 +1,25 @@
+/*
+ *Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola.mundo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author aguinaldogonzalez
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Hola mundo");
+        // TODO code application logic here
+    }
+    
+}
